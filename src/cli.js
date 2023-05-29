c915fb54ed4855a4ff345cfe8a61ae71df884851
KristinaDegtereva <kristalluna@yandex.ru>
2023-05-29 19:55:21 +0300

    update

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
 
-export const userName = readlineSync.question('May I have your name? ');
+const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName} !`);
 
-// export default readlineSync;
-
+export default userName;
