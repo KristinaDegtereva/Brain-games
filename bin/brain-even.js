3a2814934cac2d5280b8f3b006f4f936288ebaa3
KristinaDegtereva <kristalluna@yandex.ru>
2023-05-30 19:17:51 +0300

    fixed code

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,43 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync'
-
-const greeting = () => {
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
-};
-export default greeting;
-
-greeting();
-
-const randomNum = Math.floor(Math.random() * 100);
-
-console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
-console.log(`Question: ${randomNum}`);
-
-const answer = readlineSync.question('Your answer: ');
-
-const startRounds = () => {
-const correctAnswer = randomNum % 2 === 0 ? 'yes' : 'no';
-
-if (answer === correctAnswer) {
-    console.log('Correct!');
-  return true;
-  } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was ${correctAnswer}`);
-    return false;
-  }
-}
-const startGame = () => {
-    for (let i = 0; i < 3; i+= 1 ) {
-      const isCorrect = startRounds();
-      
-      if (isCorrect === false) {
-        console.log(`Let's try again, ${userName}`);
-        return;
-      }  
-    }
-    console.log(`Congratulations, ${userName}`);
-  };
+import startGame from '../src/even.js';
 
 startGame();
