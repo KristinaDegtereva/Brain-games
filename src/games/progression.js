912e0f401df34ad72be1e15d02cd6dda0bbadd9c
KristinaDegtereva <kristalluna@yandex.ru>
2023-06-05 11:16:34 +0300

    update progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,12 +5,12 @@ export const rules = 'What number is missing in the progression?';
 
 export const startRounds = () => {
   let firstNum = randomNum();
-  const prog = [];
+  const prog = [firstNum];
   const num = randomNum();
   const hiddenNum = Math.floor(Math.random() * 11);
 
   const numProgression = () => {
-    for (let i = 0; i < 10; i += 1) {
+    for (let i = 0; i < 9; i += 1) {
       const nextNum = firstNum + num;
       firstNum = nextNum;
       prog.push(nextNum);
