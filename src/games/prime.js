ce66250072ef4dda5dcb750cd0ab41f21139e2cf
KristinaDegtereva <kristalluna@yandex.ru>
2023-06-12 20:04:23 +0300

    update code

--- src/games/prime.js
+++ src/games/prime.js
@@ -10,7 +10,7 @@ const isPrime = (num) => {
     return false;
   }
 
-  for (let i = 2; i < sqrt; i += 1) {
+  for (let i = 2; i <= sqrt; i += 1) {
     if (num % i === 0) {
       return false;
     }
